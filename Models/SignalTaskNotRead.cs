0a96e6e27a26d440b74a38c6eeb979d6b0d82c54
Uğur Ortaç <ugur@meyer.com.tr>
2019-05-07 13:17:29 +0300

    .

--- Models/SignalTaskNotRead.cs
+++ Models/SignalTaskNotRead.cs
@@ -7,10 +7,10 @@ namespace IdenfitGuardware.Client.Models
         public Guid DeviceId { get; set; }
         public Guid BranchId { get; set; }
         public Guid? RecordId { get; set; }
+        public DateTime Date { get; set; }
         public string StationName { get; set; }
         public string NotReadName { get; set; }
         public string NotReadDescription { get; set; }
-
         public string TaskName { get; set; }
         public string Employee { get; set; }
 
